39eb7c62cf52c846d869a7c68675a05e45fdecd5
AntoanNikolov <antoanameya@gmail.com>
2024-11-16 14:24:05 -0500

    Card class should be finished

--- old_maid_backend.py
+++ old_maid_backend.py
@@ -8,6 +8,10 @@ class Card:
     def __init__(self, rank, suit):
         self.rank = rank
         self.suit = suit
+
+    def is_equal(self, other):
+        return self.rank == other.rank #compare ranks for forming matches
+
     
     def __str__(self):
         return f"{self.rank}{self.suit}"
@@ -18,6 +22,9 @@ class Deck:
         for suit in Card.SUITS:  #for every suit
             for rank in Card.RANKS:  #for every number value
                 self.cards.append(Card(rank, suit))  #add a card to the deck with that rank and suit
+    
+    def shuffle_deck(self):
+        random.shuffle(self.cards) #https://www.w3schools.com/python/ref_random_shuffle.asp
 
 
 class Hand: #(player)

ec3bc5cad102c1b8720cc1da237ca33c91b6686d
AntoanNikolov <antoanameya@gmail.com>
2024-11-16 12:59:14 -0500

    Created classes the game will use

---
+++ old_maid_backend.py
@@ -0,0 +1,14 @@
+import random #random card selections, etc.
+import time #will use this to make the game run at an enjoyable pace
+
+class Card:
+    pass
+
+class Deck:
+    pass
+
+class Hand:
+    pass
+
+class Game:
+    pass
